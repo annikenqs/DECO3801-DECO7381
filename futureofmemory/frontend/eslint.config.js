a2aa62866b02a2ec52cbd2cc063a4184704a9567
Johanne GG <johanne.gjedrem@icloud.com>
2025-09-01 16:10:57 +1000

    Configure eslint

---
+++ futureofmemory/frontend/eslint.config.js
@@ -0,0 +1,29 @@
+// eslint.config.js
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import globals from "globals";
+
+export default [
+  {
+    ignores: ["dist"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      "prettier/prettier": "error",
+    },
+  },
+  prettier, // disable ESLint rules that conflict with Prettier
+];
