cb727a2ef754f170b6ccb776f21ec6737998e8bb
Jiani-YANG-11 <141114154+Jiani-YANG-11@users.noreply.github.com>
2025-09-09 15:12:09 +1000

    create basic scenario1

--- futureofmemory/frontend/src/moon.js
+++ futureofmemory/frontend/src/moon.js
@@ -1,15 +1,15 @@
 const text = "It's 2100...\nYou are the last human alive...";
 let index = 0;
-const speed = 50; // typing speed
+const speed = 50; 
 
 function typeWriter() {
-  if (index < text.length) {
-    document.getElementById('story').innerHTML += text.charAt(index);
-    index++;
-    setTimeout(typeWriter, speed);
-  } else {
-    document.getElementById('next-btn').style.display = 'inline-block'; // 显示按钮
-  }
+    if (index < text.length) {
+        document.getElementById("story").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, speed);
+    } else {
+        document.getElementById("next-btn").style.display = "inline-block"; 
+    }
 }
 
 window.onload = typeWriter;
