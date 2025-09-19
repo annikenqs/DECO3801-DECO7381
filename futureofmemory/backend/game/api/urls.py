bf5305f4996672e6e0039ab0132dbc182131ec11
Samuel Chung <samuelchung@d-i89-138-202.student.eduroam.uq.edu.au>
2025-09-19 10:23:59 +1000

    made import changes to ensure testing works

--- futureofmemory/backend/game/api/urls.py
+++ futureofmemory/backend/game/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .api import views
+from ..api import views
 
 urlpatterns = [
     # path('', views.index, name='index'),
--- futureofmemory/backend/server/urls.py
+++ futureofmemory/backend/server/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('game.urls')),
+    path('', include('game.api.urls')),
     path('accounts/', include('django.contrib.auth.urls')),  # /login, /logout
 ]
