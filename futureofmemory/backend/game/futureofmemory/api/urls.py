4f80842f3069137aa8a99f6b07e6b97eb0e2ed07
Anniken Strand <annikeqs@stud.ntnu.no>
2025-09-21 19:38:13 +1000

    Remove unused code

--- futureofmemory/backend/game/futureofmemory/api/urls.py
+++ futureofmemory/backend/game/futureofmemory/api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-# from ..api import views
 from .views import ScenarioView, ChoicesView, OutcomeView
 
 urlpatterns = [
@@ -7,10 +6,4 @@ urlpatterns = [
     path("choices/", ChoicesView.as_view(), name="choices"),
     path("outcome/", OutcomeView.as_view(), name="outcome"),
     
-    # path('', views.index, name='index'),
-    # path('api/ping/', views.ping, name='ping'),
-    # path('api/select-faction/', views.select_faction),
-    # path('api/questions/', views.questions_for_player),
-    # path('api/answer/', views.submit_answer),
-    # path('api/answers/', views.my_answers),
 ]
