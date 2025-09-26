cb20ce60f376540952cd05088a3eca70125c29de
Samuel Chung <samuelchung@Samuels-Air.modem>
2025-09-26 16:42:07 +1000

    update urls.py as well

--- futureofmemory/backend/game/futureofmemory/api/urls.py
+++ futureofmemory/backend/game/futureofmemory/api/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from .views import SessionView, ScenarioView, ChoiceView, FactionView
+from .views import (
+    SessionView,
+    FactionView,
+    ScenarioView,
+    ChoiceView,
+    LobbyStatusView,
+    LobbyJoinView,
+)
 
 urlpatterns = [
-    path("session/", SessionView.as_view(), name="session"),
-    path("session/<str:session_id>/scenario/", ScenarioView.as_view(), name="scenario"),
-    path("session/<str:session_id>/choice/", ChoiceView.as_view(), name="choice"),
-    path("session/<str:session_id>/faction", FactionView.as_view(), name="faction"),
+    path("session/", SessionView.as_view(), name="create-session"),
+    path("session/<str:pin>/faction/", FactionView.as_view(), name="faction"),
+    path("session/<str:pin>/scenario/", ScenarioView.as_view(), name="scenario"),
+    path("session/<str:pin>/choice/", ChoiceView.as_view(), name="choice"),
+    path("session/<str:pin>/status/", LobbyStatusView.as_view(), name="lobby-status"),
+    path("session/<str:pin>/join/", LobbyJoinView.as_view(), name="lobby-join"),
 ]
